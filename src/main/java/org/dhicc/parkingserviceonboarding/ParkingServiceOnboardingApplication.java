c152e4c91802f085a19c2a889e666566e5a96248
김희석 <hglad0216@gmail.com>
2025-02-25 17:38:24 +0900

    feat : app 에 스캐쥴링 어노테이션 추가

--- src/main/java/org/dhicc/parkingserviceonboarding/ParkingServiceOnboardingApplication.java
+++ src/main/java/org/dhicc/parkingserviceonboarding/ParkingServiceOnboardingApplication.java
@@ -2,10 +2,12 @@ package org.dhicc.parkingserviceonboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class ParkingServiceOnboardingApplication {
 
     public static void main(String[] args) {
