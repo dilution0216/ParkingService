d11ca964cf5b4dc92384640d8632361c1295467a
김희석 <hglad0216@gmail.com>
2025-03-04 14:43:47 +0900

    test : 웹훅 테스트

--- src/main/java/org/dhicc/parkingserviceonboarding/config/ApplicationConfig.java
+++ src/main/java/org/dhicc/parkingserviceonboarding/config/ApplicationConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @EnableAsync  //  비동기 처리 활성화
 public class ApplicationConfig {
+    //t
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
